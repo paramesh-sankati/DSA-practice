94c2aeae2a015b28ba3dbec8b0c4eb82f5ec5e31
paramesh-sankati <parameshyadav865@gmail.com>
2025-03-24 21:28:11 +0530

    NumPattern

---
+++ Patterns/NumPatternHard1.py
@@ -0,0 +1,14 @@
+n=5
+t=1
+for i in range(n,0,-1):
+    temp=t
+    c=i
+    for j in range(n-i+1):
+        if i+j==n:
+            print(j+1,end=" ")
+        else:
+            print(temp,end=" ")
+            temp=temp-c
+        c=c+1
+    t+=i
+    print()    
