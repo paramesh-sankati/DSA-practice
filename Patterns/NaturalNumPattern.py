30c94632c0a3c8f334a0642aced2ab555cb17c5a
paramesh-sankati <parameshyadav865@gmail.com>
2025-03-26 21:13:44 +0530

    NaturalNumPatterns

---
+++ Patterns/NaturalNumPattern.py
@@ -0,0 +1,14 @@
+n=5
+lst=[0]
+temp=list(range(n-1,0,-1))
+lst.extend(temp)
+c=1
+for i in range(1,n+1):
+    m=c
+    for j in range(i):
+        m=m+lst[j]
+        print(m,end=" ")
+    c+=1
+
+    print()
+
