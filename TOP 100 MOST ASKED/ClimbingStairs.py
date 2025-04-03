9295ac68dfe6d64c1c899c1948e1b1c564de5a20
paramesh-sankati <parameshyadav865@gmail.com>
2025-04-03 19:23:28 +0530

    DSA

--- TOP 100 MOST ASKED/ClimbingStairs.py
+++ TOP 100 MOST ASKED/ClimbingStairs.py
@@ -3,8 +3,6 @@ You are climbing a staircase. It takes n steps to reach the top.
 
 Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
- 
-
 Example 1:
 
 Input: n = 2
@@ -20,4 +18,11 @@ Explanation: There are three ways to climb to the top.
 1. 1 step + 1 step + 1 step
 2. 1 step + 2 steps
 3. 2 steps + 1 step
+
 '''
+n=5
+arr=[0,1,2]
+for i in range(3,n+1):
+    arr.append(arr[i-1]+arr[i-2])
+print(arr[n])
+
